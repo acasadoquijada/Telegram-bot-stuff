e565ae82fa5ff8f746c78222d9a69f5ee1b569b5
Alejandro Casado Quijada <acasadoquijada@gmail.com>
2015-09-07 19:53:55 +0200

    Update exception_log.py

--- Stuff/exception_log.py
+++ Stuff/exception_log.py
@@ -1,8 +1,26 @@
-def exceptions_log(e,funtion):
+
+#Excepcion log
+
+# e -> The exception
+# m -> Message that caused the exception
+
+def exception_log(e,m):
     hour = time.strftime("%H:%M:%S")
     date = time.strftime("%d/%m/%y")
+        
+    complete_date = 'Times exception':  + str(date) + ' ' + str(hour) + '\n'
     
-    information = "[" + str(date) + ' ' + str(hour) + ' ' + str(funtion) + "]: " + str(e) 
+    cause = 'Cause of the exception: ' + str(m.text) + '\n'
+    
+    exception = 'Exception: ' + str(e)
+    
+    information = complete_date + cause + exception
+    
+    
     aux = open( 'exception.txt', 'a') 
-    aux.write( str(information) + "\n")
+    aux.write( str(information) + "\n\n")
     aux.close()
+    
+    
+## Output
+#
