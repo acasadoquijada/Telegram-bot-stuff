c05b9286ca2339765e3f95848f08f77f7bce6b78
lulivi <luislivi94@gmail.com>
2015-09-07 20:03:03 +0200

    Updated functionality
    Fixed Error

--- Stuff/search_users.py
+++ Stuff/search_users.py
@@ -1,14 +1,17 @@
 ################################################################################
 # search_users function
 ################################################################################
+
 def search_user(m):
-    text = m.text
+    pos = m.text.find(" ")
+    text = m.text[pos:]
     pos1 = text.find("@")
     if pos1 != -1:
         pos2 = text.find(" ",pos1)
         user_name = text[pos1:pos2]
         return user_name
     return -1
+
 
 ################################################################################
 # An example of use can be:
