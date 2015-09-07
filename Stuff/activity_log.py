1221622c3dbe62afd016c9295414915f15433ad5
Alejandro Casado Quijada <acasadoquijada@gmail.com>
2015-09-07 18:58:51 +0200

    Update activity_log.py

--- Stuff/activity_log.py
+++ Stuff/activity_log.py
@@ -1,14 +1,20 @@
-#Activity log
+##########################
+#       Activity log     #
+##########################
+
 def log(m):
     user_id = m.from_user.id
-    user_name = m.from_user.first_name
-    user_last_name = m.from_user.last_name
-    user_alias = m.from_user.username
+    name = m.from_user.first_name
+    last_name = m.from_user.last_name
+    username = m.from_user.username
     hour = time.strftime("%H:%M:%S")
     date = time.strftime("%d/%m/%y")
     information = "["+ str(date) + ' ' + str(hour) + ' ' +str(user_name)  + ' ' + 
-    str(user_last_name) + ' ' + str(user_alias) + ' @' + str(alias) + "]: " + m.text 
+    str(last_name) + ' ' + str(user_id) + ' @' + str(username) + "]: " + m.text 
     
     aux = open( 'log.txt', 'a')
     aux.write( str(information) + "\n")
     aux.close()
+
+## An example of the output
+## [02/09/15 11:51:34 name last_name id username]: /command text
